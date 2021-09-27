d2081a6f6f13dc9de464386f6cc67d5fe44c59bb
den4ik 124 <den4ik124@gmail.com>
2021-09-27 22:35:51 +0300

    Кооментарий к Логгеру

--- BankUI/HelpClasses/Logger.cs
+++ BankUI/HelpClasses/Logger.cs
@@ -4,6 +4,7 @@ namespace BankUI.HelpClasses
 {
     internal static class Logger
     {
+        // TODO Добавить правильное логирование вместо MessageBox
         public static void OnTransactionCreated()
         {
             MessageBox.Show("Событие отработало");
