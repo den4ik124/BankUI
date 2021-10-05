a49cc779cc7a1d7a75ef1b31d6565e5a4f398985
den4ik124 <den4ik124@gmail.com>
2021-10-05 18:41:58 +0300

    В новом UI использовать ListBox вместо DataGrid

---
+++ BankUI/ViewModels/ClientsDataViewModel.cs
@@ -0,0 +1,13 @@
+﻿using BankUI.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankUI.ViewModels
+{
+    public class ClientsDataViewModel : BaseViewModel
+    {
+    }
+}
